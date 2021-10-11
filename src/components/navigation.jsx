eb82f8c07f7013135beab2a9aca287f696afcefc
Lexo456 <49648387+Lexo456@users.noreply.github.com>
2021-10-11 15:22:34 +0200

    Début premier projet REACT

---
+++ src/components/navigation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <h1>Accueil</h1>
+      <h1>A Propos</h1>
+    </div>
+  );
+};
+
+export default Navigation;
---
+++ src/pages/homepage.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="Home">
+      <h1>Test Titre</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident harum
+        nesciunt recusandae amet blanditiis quaerat, expedita, accusamus placeat
+        esse nulla a omnis perspiciatis iste! Dolor explicabo quod harum rerum
+        nemo?
+      </p>
+      <br />
+      <h1>Test Titre 2</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus
+        accusantium deserunt minima recusandae modi, aliquam quia inventore nam
+        enim voluptatum debitis, repudiandae eligendi tempore ratione non
+        quaerat ipsum quam! Asperiores, soluta consequuntur. Consequatur atque
+        voluptas veritatis eaque repellat inventore labore recusandae animi
+        nobis sed impedit magni nisi necessitatibus mollitia alias, perferendis
+        repellendus unde pariatur illo quae dolor, ratione adipisci excepturi.
+        Quo assumenda explicabo ad, facere consequatur, iure harum possimus
+        inventore, est maxime officia ratione dolorum quidem exercitationem odio
+        quasi nesciunt unde praesentium! Beatae maxime veritatis quaerat labore
+        iusto maiores praesentium sit nesciunt harum quo repudiandae adipisci
+        repellat, quisquam optio, et excepturi vel? Necessitatibus dignissimos,
+        fuga, sed esse minima pariatur vitae assumenda saepe facilis veniam
+        ipsam ad ut deserunt possimus modi aliquid natus facere delectus neque
+        obcaecati praesentium maxime, accusantium cum atque. Officiis earum
+        tempore veritatis minus atque suscipit alias eius
+      </p>
+    </div>
+  );
+};
+
+export default Home;
